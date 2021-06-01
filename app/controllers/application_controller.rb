7cc29a033319cdc1a173b60ac8fae10688313efa
Masatooo0 <masato.n.0321@gmail.com>
2021-06-01 05:18:47 +0000

    [fix]routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+
 	before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :authenticate_user!, except: [:about, :top, :public_action]
 
   protected
   def after_sign_in_path_for(resource)
-    root_path
+    user_path(resource)
   end
 
   def after_sign_out_path_for(resource)
