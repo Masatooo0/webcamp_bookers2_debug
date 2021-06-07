33b6c45bb6344960d3db524f4bd0f1f1acdfaaaa
Masatooo0 <masato.n.0321@gmail.com>
2021-06-07 12:51:39 +0000

    [add]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about',to: 'homes#about', as: 'about'
-  resources :books
+  
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
   resources :users,only: [:show,:index,:edit,:update]
 end
